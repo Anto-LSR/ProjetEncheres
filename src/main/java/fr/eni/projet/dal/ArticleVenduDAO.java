7dfc7313a28789bd751b6b539cfd98a7755877ea
alefrere2021 <alefrere2021@177SE44-18MWH42.ad.campus-eni.fr>
2022-02-01 16:25:19 +0100

    Modify ArticleVenduDAO and AticleVenduImpl

--- src/main/java/fr/eni/projet/dal/ArticleVenduDAO.java
+++ src/main/java/fr/eni/projet/dal/ArticleVenduDAO.java
@@ -6,8 +6,9 @@ import fr.eni.projet.bo.ArticleVendu;
 
 public interface ArticleVenduDAO {
 	public int insertArticle (ArticleVendu articleVendu);
-	public ArticleVendu selectBy (ArticleVendu articleVendu);
 	public List<ArticleVendu> selectAllArticles();
-	public List<ArticleVendu> selectArticleByCategorie();
+	public List<ArticleVendu> selectByLetter(String recherche);
+	public ArticleVendu selectByName (ArticleVendu articleByName);
+	public List<ArticleVendu> selectArticleByCategorie(ArticleVendu articleByCategorie);
 	
 }
--- src/main/java/fr/eni/projet/dal/jdbcImplement/ArticleVenduImpl.java
+++ src/main/java/fr/eni/projet/dal/jdbcImplement/ArticleVenduImpl.java
@@ -20,6 +20,9 @@ public class ArticleVenduImpl implements ArticleVenduDAO {
 	private final static String SQL_INSERT = " INSERT INTO ARTICLES_VENDUS (nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, no_utilisateur, no_categorie) \r\n"
 			+ "VALUES ( ? , ? , ?, ?, ?, ?, ?);";
 	private final static String SQL_SELECT_ALL = "SELECT * FROM ARTICLES_VENDUS;";
+	private final static String SQL_SELECT_BY_LETTER = "SELECT * FROM ARTICLES_VENDUS WHERE nom_article LIKE ? OR description LIKE ?;";
+	private final static String SQL_SELECT_BY_NAME = "SELECT * FROM ARTICLES_VENDUS WHERE nom_article = ?;";
+	private final static String SQL_SELECT_BY_CATEGORIE = " SELECT * FROM ARTICLES_VENDUS WHERE no_categorie = ? ;";
 
 	@Override
 	public int insertArticle(ArticleVendu articleVendu) {
@@ -57,12 +60,6 @@ public class ArticleVenduImpl implements ArticleVenduDAO {
 	}
 
 	@Override
-	public ArticleVendu selectBy(ArticleVendu articleVendu) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public List<ArticleVendu> selectAllArticles() {
 		Connection cnx = null;
 		Statement stmt = null;
@@ -88,25 +85,142 @@ public class ArticleVenduImpl implements ArticleVenduDAO {
 					articleVendu.setDateFinEncheres(rs.getDate("date_fin_encheres").toLocalDate());
 					Utilisateur utilisateur = new Utilisateur();
 					utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+					articleVendu.setUtilisateurVendeur(utilisateur);
 					Categorie categorie = new Categorie();
 					categorie.setNoCategorie(rs.getInt("no_categorie"));
 					articleVendu.setCategorie(categorie);
-					
+
 					liste_articles.add(articleVendu);
 				}
 				return liste_articles;
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			ConnectionProvider.closeConnection(cnx, stmt);
+		}
+
+		return null;
+	}
+
+	public List<ArticleVendu> selectByLetter(String recherche) {
+		Connection cnx = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArticleVendu articleVendu = new ArticleVendu();
+		List<ArticleVendu> liste_ByLetter = new ArrayList<>();
+
+		try {
+			cnx = ConnectionProvider.getConnection();
+			pstmt = cnx.prepareStatement(SQL_SELECT_BY_LETTER);
+			pstmt.setString(1, recherche);
+			pstmt.setString(2, recherche);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				articleVendu.setNoArticle(articleVendu.getNoArticle());
+				articleVendu.setNomArticle(articleVendu.getNomArticle());
+				articleVendu.setDescription(articleVendu.getDescription());
+				articleVendu.setDateDebutEncheres(articleVendu.getDateDebutEncheres());
+				articleVendu.setDateFinEncheres(articleVendu.getDateFinEncheres());
+				articleVendu.setPrixInitial(articleVendu.getPrixInitial());
+				articleVendu.setPrixVente(articleVendu.getPrixVente());
+				Utilisateur utilisateur = new Utilisateur();
+				utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+				articleVendu.setUtilisateurVendeur(utilisateur);
+				Categorie categorie = new Categorie();
+				categorie.setNoCategorie(rs.getInt("no_categorie"));
+				articleVendu.setCategorie(categorie);
+
+				liste_ByLetter.add(articleVendu);
+			}
+
+			return liste_ByLetter;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionProvider.closeConnection(cnx, pstmt);
 		}
 
 		return null;
 	}
 
 	@Override
-	public List<ArticleVendu> selectArticleByCategorie() {
-		// TODO Auto-generated method stub
+	public ArticleVendu selectByName(ArticleVendu articleByName) {
+		Connection cnx = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArticleVendu articleVendu = new ArticleVendu();
+
+		try {
+			cnx = ConnectionProvider.getConnection();
+			pstmt = cnx.prepareStatement(SQL_SELECT_BY_NAME);
+			pstmt.setString(1, articleByName.getNomArticle());
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				articleVendu.setNoArticle(articleVendu.getNoArticle());
+				articleVendu.setNomArticle(articleVendu.getNomArticle());
+				articleVendu.setDescription(articleVendu.getDescription());
+				articleVendu.setDateDebutEncheres(articleVendu.getDateDebutEncheres());
+				articleVendu.setDateFinEncheres(articleVendu.getDateFinEncheres());
+				articleVendu.setPrixInitial(articleVendu.getPrixInitial());
+				articleVendu.setPrixVente(articleVendu.getPrixVente());
+				Utilisateur utilisateur = new Utilisateur();
+				utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+				articleVendu.setUtilisateurVendeur(utilisateur);
+				Categorie categorie = new Categorie();
+				categorie.setNoCategorie(rs.getInt("no_categorie"));
+				articleVendu.setCategorie(categorie);
+
+				return articleVendu;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionProvider.closeConnection(cnx, pstmt);
+		}
+		return null;
+	}
+
+	@Override
+	public List<ArticleVendu> selectArticleByCategorie(ArticleVendu articleByCategorie) {
+		Connection cnx = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<ArticleVendu> liste_ByCategorie = new ArrayList<>();
+
+		try {
+			cnx = ConnectionProvider.getConnection();
+			pstmt = cnx.prepareStatement(SQL_SELECT_BY_CATEGORIE);
+			pstmt.setInt(1, articleByCategorie.getCategorie().getNoCategorie());
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				ArticleVendu articleVendu = new ArticleVendu();
+				articleVendu.setNoArticle(rs.getInt("no_article"));
+				articleVendu.setDescription(rs.getString("description"));
+				articleVendu.setDateDebutEncheres(rs.getDate("date_debut_encheres").toLocalDate());
+				articleVendu.setDateFinEncheres(rs.getDate("date_fin_encheres").toLocalDate());
+				articleVendu.setPrixInitial(rs.getInt("prix_initial"));
+				articleVendu.setPrixVente(rs.getInt("prix_vente"));
+				Utilisateur utilisateur = new Utilisateur();
+				utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+				articleVendu.setUtilisateurVendeur(utilisateur);
+				Categorie categorie = new Categorie();
+				categorie.setNoCategorie(rs.getInt("no_categorie"));
+				articleVendu.setCategorie(categorie);
+
+				liste_ByCategorie.add(articleVendu);
+			}
+			return liste_ByCategorie;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionProvider.closeConnection(cnx, pstmt);
+		}
+
 		return null;
 	}
 
