266c37152e4087ef0545bb393d6fdd48a5c69855
alesueur2021 <alesueur2021@177SE44-F8MWH42.ad.campus-eni.fr>
2022-02-01 08:59:44 +0100

    Added categories

--- src/main/java/fr/eni/projet/bo/Categorie.java
+++ src/main/java/fr/eni/projet/bo/Categorie.java
@@ -1,9 +1,54 @@
 package fr.eni.projet.bo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Categorie {
-	private  int noCategorie;
+	private int noCategorie;
 	private String libelle;
 	private List<ArticleVendu> articles = null;
+	
+	//Constructeurs
+	public Categorie() {
+		this.articles = new ArrayList();
+	}
+
+	public Categorie(int noCategorie, String libelle, List<ArticleVendu> articles) {
+		this.articles = new ArrayList();
+		setNoCategorie(noCategorie);
+		setLibelle(libelle);
+		setArticles(articles);
+	}
+
+	
+	//Getters/setters
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public List<ArticleVendu> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(List<ArticleVendu> articles) {
+		this.articles = articles;
+	}
+	
+	//Méthode d'ajout à la liste
+	public void addToArticles(ArticleVendu article) {
+		this.articles.add(article);
+	}
+
 }
