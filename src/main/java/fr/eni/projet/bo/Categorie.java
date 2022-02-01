d0b7b6b06f0f0bbb99039d60813bd6494af290bc
alesueur2021 <alesueur2021@177SE44-F8MWH42.ad.campus-eni.fr>
2022-02-01 08:45:39 +0100

    categories ongoing

---
+++ src/main/java/fr/eni/projet/bo/Categorie.java
@@ -0,0 +1,9 @@
+package fr.eni.projet.bo;
+
+import java.util.List;
+
+public class Categorie {
+	private  int noCategorie;
+	private String libelle;
+	private List<ArticleVendu> articles = null;
+}
--- src/main/java/fr/eni/projet/bo/Enchere.java
+++ src/main/java/fr/eni/projet/bo/Enchere.java
@@ -1,41 +1,53 @@
 package fr.eni.projet.bo;
+
 import java.time.LocalDate;
+
 public class Enchere {
-    private LocalDate dateEnchere;
-    private int montantEnchere;
-    private Utilisateur utilisateur;
-    private ArticleVendu articlevendu;
-    public Enchere(LocalDate dateEnchere, int montantEnchere, Utilisateur utilisateur, ArticleVendu articlevendu) {
-        super();
-        setDateEnchere(dateEnchere);
-        setMontantEnchere(montantEnchere);
-        setUtilisateur(utilisateur);
-        setArticlevendu(articlevendu);
-    }
-    public Enchere() {
-    }
-    public LocalDate getDateEnchere() {
-        return dateEnchere;
-    }
-    public void setDateEnchere(LocalDate dateEnchere) {
-        this.dateEnchere = dateEnchere;
-    }
-    public int getMontantEnchere() {
-        return montantEnchere;
-    }
-    public void setMontantEnchere(int montantEnchere) {
-        this.montantEnchere = montantEnchere;
-    }
-    public Utilisateur getUtilisateur() {
-        return utilisateur;
-    }
-    public void setUtilisateur(Utilisateur utilisateur) {
-        this.utilisateur = utilisateur;
-    }
-    public ArticleVendu getArticlevendu() {
-        return articlevendu;
-    }
-    public void setArticlevendu(ArticleVendu articlevendu) {
-        this.articlevendu = articlevendu;
-    }
+	private LocalDate dateEnchere;
+	private int montantEnchere;
+	private Utilisateur utilisateur;
+	private ArticleVendu articlevendu;
+
+	public Enchere() {
+	}
+
+	public Enchere(LocalDate dateEnchere, int montantEnchere, Utilisateur utilisateur, ArticleVendu articlevendu) {
+
+		setDateEnchere(dateEnchere);
+		setMontantEnchere(montantEnchere);
+		setUtilisateur(utilisateur);
+		setArticlevendu(articlevendu);
+	}
+
+	public LocalDate getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(LocalDate dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+	public ArticleVendu getArticlevendu() {
+		return articlevendu;
+	}
+
+	public void setArticlevendu(ArticleVendu articlevendu) {
+		this.articlevendu = articlevendu;
+	}
 }
