e8560796c73305b2bed782b313954794b397232d
alesueur2021 <alesueur2021@177SE44-F8MWH42.ad.campus-eni.fr>
2022-02-02 09:01:31 +0100

    fixed identification servlet

--- src/main/java/fr/eni/projet/servlets/IdentificationServlet.java
+++ src/main/java/fr/eni/projet/servlets/IdentificationServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IdentificationServlet
  */
-@WebServlet("/Identification")
+@WebServlet("/identification")
 public class IdentificationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
