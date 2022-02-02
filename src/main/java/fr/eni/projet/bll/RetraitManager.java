aef9c96eeb90e17c957d0ca4386f5615991463a3
ikalloubi2021 <ikalloubi2021@177SE46-CNVVV22.ad.campus-eni.fr>
2022-02-02 14:12:36 +0100

    Created RetraitDAO RetraitImpl RetraitManager

--- src/main/java/fr/eni/projet/bll/RetraitManager.java
+++ src/main/java/fr/eni/projet/bll/RetraitManager.java
@@ -1,5 +1,49 @@
 package fr.eni.projet.bll;
 
+import fr.eni.projet.bo.Retrait;
+import fr.eni.projet.dal.DAOFactory;
+import fr.eni.projet.dal.RetraitDAO;
+
 public class RetraitManager {
 
+	private static RetraitManager instance;
+
+	public static RetraitManager getInstance() {
+
+		if (instance == null) {
+			instance = new RetraitManager();
+
+		}
+
+		return instance;
+
+	}
+
+	public void insertRetrait(Retrait retrait) {
+		RetraitDAO rt = DAOFactory.createNewRetraitImpl();
+		rt.insertRetrait(retrait);
+
+	}
+
+	public void updateRetrait(Retrait retrait) {
+		RetraitDAO rt = DAOFactory.createNewRetraitImpl();
+		rt.updateRetrait(retrait);
+
+	}
+
+	public void deleteRetrait(Retrait retrait) {
+		RetraitDAO rt = DAOFactory.createNewRetraitImpl();
+		rt.deleteRetrait(retrait);
+	}
+
+	public Retrait selectRetraitByNo(Retrait retrait) {
+		
+		
+		RetraitDAO rt = DAOFactory.createNewRetraitImpl();
+		Retrait newretrait = rt.selectRetraitByNo(retrait);
+		
+		return newretrait;
+	}
+	
+	
 }
--- src/main/java/fr/eni/projet/dal/RetraitDAO.java
+++ src/main/java/fr/eni/projet/dal/RetraitDAO.java
@@ -1,5 +1,15 @@
 package fr.eni.projet.dal;
 
+import fr.eni.projet.bo.Retrait;
+
 public interface RetraitDAO {
+
+	public void insertRetrait(Retrait retrait);
+
+	public Retrait selectRetraitByNo(Retrait retrait);
+
+	public void updateRetrait(Retrait retrait);
+
+	public void deleteRetrait(Retrait retrait);
 
 }
