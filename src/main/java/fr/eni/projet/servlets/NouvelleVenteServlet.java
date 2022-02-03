f750107d8739dd22f56ca67e5544b0a26d2f4b9d
alefrere2021 <alefrere2021@177SE44-18MWH42.ad.campus-eni.fr>
2022-02-03 16:40:43 +0100

    Update nouvelleVente

--- src/main/java/fr/eni/projet/servlets/NouvelleVenteServlet.java
+++ src/main/java/fr/eni/projet/servlets/NouvelleVenteServlet.java
@@ -1,11 +1,18 @@
 package fr.eni.projet.servlets;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.projet.bo.ArticleVendu;
+import fr.eni.projet.bo.Utilisateur;
 
 /**
  * Servlet implementation class NouvelleVenteServlet
@@ -26,7 +33,27 @@ public class NouvelleVenteServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		
+		boolean isConnected = true;
+		
+		if (session != null) {
+			Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+			if (utilisateur == null ) {
+				isConnected = false; 
+				session.setAttribute("connected", false);
+			} else {
+				isConnected = true;
+				session.setAttribute("connected", true);
+			}
+		}else {
+			isConnected = false;
+			request.setAttribute("connected", false);
+		}
+		if (isConnected == false) {
+			response.sendRedirect(request.getContextPath()+"/accueil");
+			System.out.println("Non Connecté page 'Nouvelle Vente' Inaccesible ");
+		}
 		request.getRequestDispatcher("/WEB-INF/jsp/nouvelleVente.jsp").forward(request, response);
 	}
 
@@ -34,7 +61,25 @@ public class NouvelleVenteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		 String nomArticle = request.getParameter("nomArticle");
+		 String description = request.getParameter("description");
+		 String dateDebutEnchere = request.getParameter("debutEnchere");
+		 String dateFinEnchere = request.getParameter("finEnchere");
+		 String prixInitial = request.getParameter("prixInitial");
+
+		 ArticleVendu newArticle = new ArticleVendu();
+		 newArticle.setNomArticle(nomArticle);
+		 newArticle.setDescription(description);
+		 LocalDate dateDebut = LocalDate.parse(dateDebutEnchere);
+		 newArticle.setDateDebutEncheres(dateDebut);
+		 LocalDate dateFin = LocalDate.parse(dateFinEnchere);
+		 newArticle.setDateFinEncheres(dateFin);
+		 int prix = Integer.parseInt(prixInitial);
+		 newArticle.setPrixInitial(prix);
+		 
+		
+		
+		
 		doGet(request, response);
 	}
 
