23fdf395f86b38e86ae27fd32f743997cc17a958
Antonin Lesueur <anton1.lesueur@gmail.com>
2022-02-06 19:40:03 +0100

    fixed register js + changed error color

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -1,5 +1,5 @@
 //******************************************************* */
-//VERIFICATION DES INPUTS SUR MODIF PROFIL
+//VERIFICATION DES INPUTS SUR INSCRIPTION 
 
 let phoneField = document.getElementById("telephone");
 let pseudoField = document.getElementById("pseudo");
@@ -17,119 +17,134 @@ let confirmBtn = document.getElementById("confirmBtn");
 let page = document.querySelector(".container");
 
 page.addEventListener("click", () => {
-  verifInput();
+	verifInput();
 });
 
 page.addEventListener("keydown", () => {
-  verifInput();
-  console.log("cocu");
+	verifInput();
+	console.log("cocu");
 });
 let error = false;
 function verifInput() {
-  error = false;
-  verifPseudoField();
-  verifNomField();
-  verifPrenomField();
-  verifMailField();
-  verifRueField();
-  verifCodePostalField();
-  verifVilleField();
-  verifPassword();
-  if (!error) {
-    confirmBtn.removeAttribute("disabled", true);
-    pseudoField.classList.remove("inputError");
-    nomField.classList.remove("inputError");
-    prenomField.classList.remove("inputError");
-    mailField.classList.remove("inputError");
-    rueField.classList.remove("inputError");
-    codePostalField.classList.remove("inputError");
-    villeField.classList.remove("inputError");
-  }
+	error = false;
+	verifPseudoField();
+	verifNomField();
+	verifPrenomField();
+	verifMailField();
+	verifRueField();
+	verifCodePostalField();
+	verifVilleField();
+	verifPassword();
+	verifEmptyPassword();
+	if (!error) {
+		confirmBtn.removeAttribute("disabled", true);
+		pseudoField.classList.remove("inputError");
+		nomField.classList.remove("inputError");
+		prenomField.classList.remove("inputError");
+		mailField.classList.remove("inputError");
+		rueField.classList.remove("inputError");
+		codePostalField.classList.remove("inputError");
+		villeField.classList.remove("inputError");
+	}
 }
 
 function verifPseudoField() {
-  if (pseudoField.value.length == 0) {
-    pseudoField.classList.add("inputError");
+	if (pseudoField.value.length == 0) {
+		pseudoField.classList.add("inputError");
 
-    confirmBtn.setAttribute("disabled", true);
-    return (error = true);
-  } else {
-    pseudoField.classList.remove("inputError");
-  }
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		pseudoField.classList.remove("inputError");
+	}
 }
 
 function verifNomField() {
-  if (nomField.value.length == 0) {
-    nomField.classList.add("inputError");
+	if (nomField.value.length == 0) {
+		nomField.classList.add("inputError");
 
-    confirmBtn.setAttribute("disabled", true);
-    return (error = true);
-  } else {
-    nomField.classList.remove("inputError");
-  }
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		nomField.classList.remove("inputError");
+	}
 }
 
 function verifPrenomField() {
-  if (prenomField.value.length == 0) {
-    prenomField.classList.add("inputError");
+	if (prenomField.value.length == 0) {
+		prenomField.classList.add("inputError");
 
-    confirmBtn.setAttribute("disabled", true);
-    return (error = true);
-  } else {
-    prenomField.classList.remove("inputError");
-  }
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		prenomField.classList.remove("inputError");
+	}
 }
 
 function verifMailField() {
-  if (mailField.value.length == 0) {
-    mailField.classList.add("inputError");
+	if (mailField.value.length == 0) {
+		mailField.classList.add("inputError");
 
-    confirmBtn.setAttribute("disabled", true);
-    return (error = true);
-  } else {
-    mailField.classList.remove("inputError");
-  }
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		mailField.classList.remove("inputError");
+	}
 }
 
 function verifRueField() {
-  if (rueField.value.length == 0) {
-    rueField.classList.add("inputError");
+	if (rueField.value.length == 0) {
+		rueField.classList.add("inputError");
 
-    confirmBtn.setAttribute("disabled", true);
-    return (error = true);
-  } else {
-    rueField.classList.remove("inputError");
-  }
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		rueField.classList.remove("inputError");
+	}
 }
 
 function verifCodePostalField() {
-  if (codePostalField.value.length == 0) {
-    codePostalField.classList.add("inputError");
+	if (codePostalField.value.length == 0) {
+		codePostalField.classList.add("inputError");
 
-    confirmBtn.setAttribute("disabled", true);
-    return (error = true);
-  } else {
-    codePostalField.classList.remove("inputError");
-  }
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		codePostalField.classList.remove("inputError");
+	}
 }
 
 function verifVilleField() {
-  if (villeField.value.length == 0) {
-    villeField.classList.add("inputError");
+	if (villeField.value.length == 0) {
+		villeField.classList.add("inputError");
 
-    confirmBtn.setAttribute("disabled", true);
-    return (error = true);
-  } else {
-    villeField.classList.remove("inputError");
-  }
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		villeField.classList.remove("inputError");
+	}
 }
 
 function verifPassword() {
-  if (password.value != confirmation.value) {
-    confirmation.classList.add("inputError");
-    confirmBtn.setAttribute("disabled", true);
-    return (error = true);
-  } else {
-    confirmation.classList.remove("inputError"); 
-  }
+	if (password.value != confirmation.value) {
+		confirmation.classList.add("inputError");
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		confirmation.classList.remove("inputError");
+	}
 }
+
+function verifEmptyPassword() {
+	if (password.value.length == 0) {
+		password.classList.add("inputError");
+
+		confirmBtn.setAttribute("disabled", true);
+		return (error = true);
+	} else {
+		password.classList.remove("inputError");
+	}
+
+}
+
+
